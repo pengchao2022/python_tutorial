1d04bfd7992187577cd1eb870e07ed57b5448ebb
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 12:46:00 +0800

    a new calculator

---
+++ if_else 练习/calculator.py
@@ -0,0 +1,20 @@
+#计算机 一个简单的计算器程序
+
+operator = input("请输入运算符：(加法：+, 减法：-, 乘法：*, 除法：/, 取余：%)\n")
+num1 = float(input("请输入index第一个数字："))
+num2 = float(input("请输入您的第二个数字："))
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    result = num1 / num2
+elif operator == '%':
+    result = num1 % num2
+else:
+    print("运算符号输入有误！")
+print(f"运算结果是：{result}")
+
