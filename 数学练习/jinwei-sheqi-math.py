01d2c09e8545d55f354a92662fe5b0e731f12272
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 11:24:10 +0800

    wutiaojian jinwei sheqi math func

---
+++ 数学练习/jinwei-sheqi-math.py
@@ -0,0 +1,22 @@
+#调用数学 math 模块中的函数
+
+import math
+#python 中的数学函数
+
+
+a = 9.98
+#无条件进位函数ceil
+print(math.ceil(a))
+
+b = 8.06
+#无条件进位函数ceil
+print(math.ceil(b))
+
+c = 9.97
+#无条件舍弃函数 floor
+print(math.floor(c))
+
+d = 101.8777
+#无条件舍弃函数 floor
+print(math.floor(d))
+
