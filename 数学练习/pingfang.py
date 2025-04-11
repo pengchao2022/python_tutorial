51c2abab0c8f42acb466d3759255b48e61ac8d97
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 10:51:48 +0800

    added

--- 数学练习/pingfang.py
+++ 数学练习/pingfang.py
@@ -1,13 +1,15 @@
 #平方练习
 
-apples = 4
+apples = 2
 apples = apples * apples
 print(apples)
 
 oranges = 6
-oranges **= 2
+#平方计算，平方后面数字是2 ，依次类推
+oranges **= 2  
 print(oranges)
 
 bananas = 5
-bananas **= 3
+#立方计算 ，立方后面数字是3 ，依次类推
+bananas **= 3   
 print(bananas)
