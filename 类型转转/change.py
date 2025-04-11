8151eeeb082a5e460d354d94d4a1568a174c35af
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 09:31:35 +0800

    Added casting change

---
+++ 类型转转/change.py
@@ -0,0 +1,23 @@
+#类型转换 type casting
+
+name = "Jim Li"
+age = 34
+mpc = 1.6
+university = True
+
+print(type(name))
+print(type(age))
+print(type(mpc))
+print(type(university))
+
+age = float(age)
+print(age)
+print(type(age))
+
+mpc = int(mpc)
+print(mpc)
+print(type(mpc))
+
+university = str(university)
+print(type(university))
+print(university)
