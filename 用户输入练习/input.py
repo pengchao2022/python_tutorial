6b808cd685ea2ef72c2f47a25bdbb8163187d334
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 09:49:25 +0800

    added input1

---
+++ 用户输入练习/input.py
@@ -0,0 +1,17 @@
+#填词游戏练习
+
+adj1 = input("请输入第一个形容词：")
+adj2 = input("请输入第二个形容词：")
+adj3 = input("请输入第三个形容词：")
+
+noum1 = input("请输入第一个名词：")
+noum2 = input("请输入第二个名词：")
+verb1 = input("请输入第一个动词：")
+
+print(f"今天我去了一个：{noum1}")
+print(f"看到了一只{noum2}")
+print(f"它看上去很：{adj1}")
+print(f"它正在：{verb1}")
+print(f"今天天气很：{adj2}")
+print(f"我自己很：{adj3}")
+
