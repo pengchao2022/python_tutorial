8416c29222018af8b307a300a72ba173a860fae6
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 22:55:36 +0800

    added number

---
+++ 随机数练习/random2.py
@@ -0,0 +1,20 @@
+# random 猜数字游戏
+import random
+
+low = 1
+high = 100
+
+number = random.randint(low,high)
+guesses = 0
+
+while True:
+    #让玩家来输入数字
+    guess = int(input(f"请输入您猜测的在{low}到{high}之间的数字："))
+    guesses += 1
+    
+    if guess < number:
+        print("您猜的数字太小了")
+    elif guess > number:
+        print("您猜的数字太大了")
+    else:
+        print(f"恭喜你猜对了，这个数字就是:{number}")
