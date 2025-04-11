5a1be2947601bcc7b73c11251b4e4d1ce2d5fee7
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-11 10:11:42 +0800

    modified the price

--- 用户输入练习/gouwu.py
+++ 用户输入练习/gouwu.py
@@ -5,5 +5,5 @@ price = float(input("单价为："))
 quantity = int(input("您要购买几个呢：？"))
 
 total = price * quantity
-print(f"您要购买的物品是：{item},每个单价为：{price},您说要买：{quantity}个，需要支付的钱数为：{total}")
+print(f"您要购买的物品是：{item},每个单价为：{price},您说要买：{quantity}个，需要支付的钱数为：￥{total}")
 
