38dd3ed7bb8b6d39b21a9042efd0fcddb756b288
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 13:45:00 +0800

    added items

--- python中的Map 函数/python_map.py
+++ python中的Map 函数/python_map.py
@@ -11,6 +11,13 @@ to_euros = lambda data: (data[0], round(data[1]*0.82))
 store_euros = list(map(to_euros, store))
 print(store_euros)
 
+
 to_usd = lambda data: (data[0], round(data[1]/30))
 store_usds = list(map(to_usd, store))
 print(store_usds)
+
+for item in store_usds:
+    print(item)
+
+for item in store_euros:
+    print(item)
