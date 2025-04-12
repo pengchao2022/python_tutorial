ab9663b4b909ec19ff4672110d652b974841603c
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 12:58:48 +0800

    added the sort reverse function

---
+++ 排序练习/list_sort.py
@@ -0,0 +1,11 @@
+#python排序练习
+
+num_list = [1,4,9,3,2,19,30]
+print(num_list)
+#使用 sort 方法来排序
+num_list.sort()
+print(num_list)
+
+#反着排序 使用reverse 方法
+num_list.sort(reverse=True)
+print(num_list)
---
+++ 獠牙运算符/liaoya.py
@@ -0,0 +1,3 @@
+print(happy := True)
+
+print(happy := False)
