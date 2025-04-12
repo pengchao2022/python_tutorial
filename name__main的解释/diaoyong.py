0f2074e0cfb3ae962266f2c66d3c416c7f1bf8c2
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 15:27:40 +0800

    added files

---
+++ name__main的解释/diaoyong.py
@@ -0,0 +1,6 @@
+# 调用相乘模块
+
+import plus
+
+plus.plus(8, 9)
+plus.plus(45, 300)
---
+++ name__main的解释/plus.py
@@ -0,0 +1,12 @@
+# 定义一个两个数相乘的函数
+
+def plus (x, y):
+    result = x * y
+    print(f"{x} 与 {y}相乘的积为：{result}")
+
+if __name__ == '__main__':
+    plus(8, 9)
+    plus(100, 77)
+    plus(33.4, 200)
+
+
