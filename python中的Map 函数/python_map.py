d29a2a65f1c402c27c56ceb4751d156d385f0cdb
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 13:41:24 +0800

    added map function

---
+++ python中的Map 函数/python_map.py
@@ -0,0 +1,16 @@
+#练习使用python中的map 函数
+
+#声明一个列表，里面都是元组tuple
+store = [
+    ("衬衫", 20),
+    ("裤子", 40),
+    ("球鞋", 120),
+    ("帽子", 500)
+]
+to_euros = lambda data: (data[0], round(data[1]*0.82))
+store_euros = list(map(to_euros, store))
+print(store_euros)
+
+to_usd = lambda data: (data[0], round(data[1]/30))
+store_usds = list(map(to_usd, store))
+print(store_usds)
