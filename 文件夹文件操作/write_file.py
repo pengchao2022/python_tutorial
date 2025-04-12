d9916da00b5a9761036dd93d8e98f1b09bd342a4
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 08:15:24 +0800

    modified file

--- 文件夹文件操作/write_file.py
+++ 文件夹文件操作/write_file.py
@@ -4,9 +4,14 @@ str = "/Users/pengchaoma/terraform/variable.tf"
 
 text = "hello\n, 今天天气很好！"
 
+#内容写入文件
 with open(str, 'w') as file:
     file.write(text)
 
+#插入文字内容
+with open(str, 'a') as file:
+    file.write('\n Allen Ma 2025\n')
 
+#读取文件内容并打印
 with open(str) as file:
     print(file.read())
