a922053fa7e78d1e46c3e5f406926a359914f70a
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-04-12 15:19:41 +0800

    added

---
+++ name__main的解释/import_module.py
@@ -0,0 +1,6 @@
+#引用 module 模块 myfunc.py
+
+import myfunc
+
+myfunc.addnum(9, 200)
+
---
+++ name__main的解释/myfunc.py
@@ -0,0 +1,8 @@
+# 定义一个加法函数
+
+def addnum (x, y):
+    result = x + y
+    print(result)
+
+if __name__ == '__main__':
+    addnum(5, 19)
